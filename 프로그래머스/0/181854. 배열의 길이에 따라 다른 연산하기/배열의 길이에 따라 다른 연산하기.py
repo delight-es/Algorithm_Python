b22a057d14109f2f774e0d307d74dab957617cea
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-04-20 01:13:35 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
@@ -1,10 +1,9 @@
 def solution(arr, n):
     answer = arr
-    N = len(arr)
-    if N % 2: #길이-홀수
-        for i in range(0, N, 2): #짝수
+    if len(arr) % 2 == 1: # 길이-홀수
+        for i in range(0, len(arr), 2): #짝수
             answer[i] += n
-    else: #길이-짝수
-        for i in range(1, N, 2): #홀수
-            answer[i] += n
+    else: # 길이-짝수
+        for j in range(1, len(arr), 2): #홀수
+            answer[j] += n
     return answer
