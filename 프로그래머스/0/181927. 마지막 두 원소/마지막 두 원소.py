41d48a0c0b343eab786714e0636f0a8c864d2ac8
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-04-23 17:00:50 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,6 +1,2 @@
 def solution(num_list):
-    if num_list[-1] > num_list[-2]:
-        num_list.append(num_list[-1]-num_list[-2])
-    else:
-        num_list.append(2*num_list[-1])
-    return num_list
+    return num_list + [num_list[-1]-num_list[-2]] if num_list[-1] > num_list[-2] else num_list + [2*num_list[-1]]
