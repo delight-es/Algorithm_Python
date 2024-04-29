5323690eb3e4c79f5a1120081c26091d9118f472
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-04-29 15:39:39 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,10 +1,2 @@
 def solution(date1, date2):
-    if (date1[0] < date2[0]): #연도 작으면
-        return 1
-    elif (date1[0] == date2[0]): #연도 같으면
-        if (date1[1] < date2[1]): #월 작으면
-            return 1
-        elif (date1[1] == date2[1]): #월 같으면
-            if (date1[2] < date2[2]): #일 작으면
-                return 1
-    return 0
+    return 1 if date1 < date2 else 0
