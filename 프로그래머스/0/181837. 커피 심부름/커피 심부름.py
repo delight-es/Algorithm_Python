6b8060f6c9508d6e1e45aa633b09944a1217b8b8
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-04-29 19:01:26 +0900

    [level 0] Title: 커피 심부름, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -1,8 +1,7 @@
 def solution(order):
-    plus_menu = ['cafelatte', 'hotcafelatte', 'cafelattehot', 'icecafelatte', 'cafelatteice']
     total = 0
     for i in order:
         total += 4500
-        if i in plus_menu:
+        if 'cafelatte' in i:
             total += 500
     return total
