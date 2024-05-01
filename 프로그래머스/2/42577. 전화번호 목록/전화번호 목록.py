fc234551550c6822805feefd761cc1dd8ea8b3f1
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-05-02 02:44:21 +0900

    [level 2] Title: 전화번호 목록, Time: 207.27 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,12 @@
 def solution(phone_book):
-    phone_book = sorted(phone_book)
-    for p in range(1,len(phone_book)):
-        if phone_book[p].startswith(phone_book[p-1]):
-            return False
+    d = {}
+    for p in phone_book: #딕셔너리 d 정의
+        d[p] = 1
+        
+    for p in phone_book: #각 번호마다
+        num = ""
+        for n in range(len(p)-1): #맨 끝번호 빼고
+            num += p[n] #번호 한글자씩 추가
+            if (num in d): #해시에 있으면
+                return False
     return True
