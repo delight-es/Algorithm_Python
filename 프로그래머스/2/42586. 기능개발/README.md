36b3565f7cfde2f07418da22abbe056c01f78064
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-06-15 13:43:17 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,6 +1,6 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 03일 18:31:31
+2024년 06월 15일 13:43:10
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,11 +2,13 @@ from collections import deque #선입선출
 
 def solution(progresses, speeds):
     answer, q = [], deque(progresses)
+    
     while len(q) >= 1:
         job = 0
         # 한 작업씩 작업 일 추가
         for i in range(len(q)):
             q[i] += speeds[i]
+            
         # 모든 작업에 대해
         for _ in range(len(q)):
             if q[0] >= 100: #앞선 작업이 100 넘으면
@@ -15,6 +17,7 @@ def solution(progresses, speeds):
                 job += 1 # 개수+1
             else: #못 넘으면
                 break
+                
         if job != 0:
             answer.append(job)
     return answer
