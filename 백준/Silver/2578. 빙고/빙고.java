ceca7324b4adf874edc20d66b40f6f7cad36f33d
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-06-29 15:19:14 +0900

    [Silver IV] Title: 빙고, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -4,130 +4,79 @@ import java.io.*;
 /* 빙고 
  * arr: 빙고배열(2차원-1~25), say: 부른숫자배열(1차원)
  * (분석) 시뮬레이션
+ * (!) 한 값을 지우면서 빙고는 각 행,열,대각선에 대해 동시에 여러번 발생 O
  */
 
 public class Main {
-    static int arr[][] = new int[5][5], say[] = new int[25], cnt = 0;
-    static boolean checkRow[] = new boolean[5];
-    static boolean checkCol[] = new boolean[5];
-    static boolean checkDiag1 = false;
-    static boolean checkDiag2 = false;
-    
-    static boolean find(int x, int y) { //지금 갱신한 val의 행열에 대해서만
-        boolean bingo = false;
-        
-        //1) 해당 행 모두 맞췄는지 확인
-        if (!checkRow[x]) {
-            boolean rowBingo = true;
-            for (int i = 0; i < 5; i++) {
-                if (arr[x][i] != 0) {
-                    rowBingo = false;
-                    break;
-                }
-            }
-            if (rowBingo) {
-                checkRow[x] = true;
-                bingo = true;
-                cnt++;
-            }
-        }
-        
-        //2) 해당 열 모두 맞췄는지 확인
-        if (!checkCol[y]) {
-            boolean colBingo = true;
-            for (int i = 0; i < 5; i++) {
-                if (arr[i][y] != 0) {
-                    colBingo = false;
-                    break;
-                }
-            }
-            if (colBingo) {
-                checkCol[y] = true;
-                bingo = true;
-                cnt++;
-            }
-        }
-        
-        //3) 대각선 원소에 해당하면 해당 대각선 모두 맞췄는지 확인
-        //왼쪽 대각선 (0,0) (1,1) (2,2) (3,3) (4,4)
-        if (x == y && !checkDiag1) {
-            boolean diag1Bingo = true;
-            for (int i = 0; i < 5; i++) {
-                if (arr[i][i] != 0) {
-                    diag1Bingo = false;
-                    break;
-                }
-            }
-            if (diag1Bingo) {
-                checkDiag1 = true;
-                bingo = true;
-                cnt++;
-            }
-        }
-        
-        //오른쪽 대각선 (0,4) (1,3) (2,2) (3,1) (4,0)
-        if (x + y == 4 && !checkDiag2) {
-            boolean diag2Bingo = true;
-            for (int i = 0; i < 5; i++) {
-                if (arr[i][4 - i] != 0) {
-                    diag2Bingo = false;
-                    break;
-                }
-            }
-            if (diag2Bingo) {
-                checkDiag2 = true;
-                bingo = true;
-                cnt++;
-            }
-        }
-        
-        return bingo;
-    }
+	static int arr[][]=new int[5][5], 
+	say[]=new int[25], cnt=0;
 
-    static void bingo(int idx, int val) { //인덱스,값
-        int x = -1, y = -1;
-        //1) 부른 숫자 지우기
-        outer: for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (arr[i][j] == val) { //부르면 0으로 체크
-                    arr[i][j] = 0;
-                    x = i;
-                    y = j;
-                    break outer;
-                }
-            }
-        }
-
-        //2) 빙고인지 확인하고 빙고면 cnt+1
-        find(x, y);
-
-        //3) 빙고 3개 모았으면 외치고 종료
-        if (cnt >= 3) {
-            System.out.println(idx + 1);
-            System.exit(0); // 프로그램 종료
-        }
-    }
-    
-    public static void main(String[] args) throws Exception {
-        //1. 입력
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        for (int i = 0; i < 5; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 5; j++) arr[i][j] = Integer.parseInt(st.nextToken());
-        }
-        for (int i = 0; i < 5; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 5; j++) say[(5 * i) + j] = Integer.parseInt(st.nextToken());
-        }
-        cnt = 0;
-
-        //2. 처리
+	//[1] 부른 숫자 지우기
+	static void delete(int val) {
+		int x=10, y=10;
+		exit: for(int i=0; i<5; i++) {
+			for(int j=0; j<5; j++ ) {
+				if(arr[i][j] == val) { //부르면 0으로 체크
+					arr[i][j] = 0;
+					x=i;
+					y=j;
+					check(x,y,val);
+					break exit;
+				}
+			}
+		}
+	}
+	
+	//[2] 빙고 가능한지 확인
+	static void check(int x, int y, int val) { //지금 갱신한 값에 대해서만
+		//1) 해당 행 모두 맞췄는지 확인
+		int sum = 0;
+		for(int i=0; i<5; i++) sum += arr[x][i];
+		if(sum == 0) cnt++;
+		
+		//2) 해당 열 모두 맞췄는지 확인
+		sum = 0;
+		for(int i=0; i<5; i++) sum += arr[i][y];
+		if(sum == 0) cnt++;
+		
+		//3) 대각선 원소에 해당하면 해당 대각선 모두 맞췄는지 확인
+		sum = 0;
+		if (x==y) { //왼쪽 대각선 (0,0) (1,1) (2,2) (3,3) (4,4)
+			for(int i=0; i<5; i++) sum += arr[i][i];
+			if(sum == 0) cnt++;
+		}
+      
+		sum = 0;
+		if(x+y==4) { //오른쪽 대각선 (0,4) (1,3) (2,2) (3,1) (4,0)
+			for(int i=0; i<5; i++) sum += arr[i][4-i];
+			if(sum == 0) cnt++;
+		}
+	}	
+	
+	public static void main(String[] args) throws Exception{
+		//1. 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		for(int i=0; i<5; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<5; j++) arr[i][j] = Integer.parseInt(st.nextToken());
+		}
+		for(int i=0; i<5; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<5; j++) say[(5*i)+j] = Integer.parseInt(st.nextToken());
+		}
+		cnt=0;
+		
+		//2. 처리
         for (int s = 0; s < say.length; s++) {
-            bingo(s, say[s]); //idx,val
+            delete(say[s]);
+            if (cnt >= 3) {  //빙고 3개 넘으면
+                System.out.println(s+1);
+                break; //종료
+            }
         }
         
-        //3. 출력
-        br.close();
-    }
+		//3. 출력
+		br.close();
+	}
 }
