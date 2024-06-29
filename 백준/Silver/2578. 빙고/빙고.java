07b303a1190f13eb66cdafb3ddbbf4657cc73ead
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-06-29 15:01:16 +0900

    [Silver IV] Title: 빙고, Time: 140 ms, Memory: 16188 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -8,41 +8,88 @@ import java.io.*;
 
 public class Main {
     static int arr[][] = new int[5][5], say[] = new int[25], cnt = 0;
-
-    static boolean checkRow(int x) {
-        for (int i = 0; i < 5; i++) {
-            if (arr[x][i] != 0) return false;
+    static boolean checkRow[] = new boolean[5];
+    static boolean checkCol[] = new boolean[5];
+    static boolean checkDiag1 = false;
+    static boolean checkDiag2 = false;
+    
+    static boolean find(int x, int y) { //지금 갱신한 val의 행열에 대해서만
+        boolean bingo = false;
+        
+        //1) 해당 행 모두 맞췄는지 확인
+        if (!checkRow[x]) {
+            boolean rowBingo = true;
+            for (int i = 0; i < 5; i++) {
+                if (arr[x][i] != 0) {
+                    rowBingo = false;
+                    break;
+                }
+            }
+            if (rowBingo) {
+                checkRow[x] = true;
+                bingo = true;
+                cnt++;
+            }
         }
-        return true;
+        
+        //2) 해당 열 모두 맞췄는지 확인
+        if (!checkCol[y]) {
+            boolean colBingo = true;
+            for (int i = 0; i < 5; i++) {
+                if (arr[i][y] != 0) {
+                    colBingo = false;
+                    break;
+                }
+            }
+            if (colBingo) {
+                checkCol[y] = true;
+                bingo = true;
+                cnt++;
+            }
+        }
+        
+        //3) 대각선 원소에 해당하면 해당 대각선 모두 맞췄는지 확인
+        //왼쪽 대각선 (0,0) (1,1) (2,2) (3,3) (4,4)
+        if (x == y && !checkDiag1) {
+            boolean diag1Bingo = true;
+            for (int i = 0; i < 5; i++) {
+                if (arr[i][i] != 0) {
+                    diag1Bingo = false;
+                    break;
+                }
+            }
+            if (diag1Bingo) {
+                checkDiag1 = true;
+                bingo = true;
+                cnt++;
+            }
+        }
+        
+        //오른쪽 대각선 (0,4) (1,3) (2,2) (3,1) (4,0)
+        if (x + y == 4 && !checkDiag2) {
+            boolean diag2Bingo = true;
+            for (int i = 0; i < 5; i++) {
+                if (arr[i][4 - i] != 0) {
+                    diag2Bingo = false;
+                    break;
+                }
+            }
+            if (diag2Bingo) {
+                checkDiag2 = true;
+                bingo = true;
+                cnt++;
+            }
+        }
+        
+        return bingo;
     }
 
-    static boolean checkCol(int y) {
-        for (int i = 0; i < 5; i++) {
-            if (arr[i][y] != 0) return false;
-        }
-        return true;
-    }
-
-    static boolean checkDiag1() {
-        for (int i = 0; i < 5; i++) {
-            if (arr[i][i] != 0) return false;
-        }
-        return true;
-    }
-
-    static boolean checkDiag2() {
-        for (int i = 0; i < 5; i++) {
-            if (arr[i][4 - i] != 0) return false;
-        }
-        return true;
-    }
-
-    static void bingo(int idx, int val) { // 인덱스, 값
+    static void bingo(int idx, int val) { //인덱스,값
         int x = -1, y = -1;
-        // 1) 부른 숫자 지우기
+        //1) 부른 숫자 지우기
         outer: for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                if (arr[i][j] == val) { // 부르면 0으로 체크
+                if (arr[i][j] == val) { //부르면 0으로 체크
                     arr[i][j] = 0;
                     x = i;
                     y = j;
@@ -51,21 +98,18 @@ public class Main {
             }
         }
 
-        // 2) 빙고인지 확인하고 빙고면 cnt+1
-        if (checkRow(x)) cnt++;
-        if (checkCol(y)) cnt++;
-        if (x == y && checkDiag1()) cnt++;
-        if (x + y == 4 && checkDiag2()) cnt++;
+        //2) 빙고인지 확인하고 빙고면 cnt+1
+        find(x, y);
 
-        // 3) 빙고 3개 모았으면 외치고 종료
+        //3) 빙고 3개 모았으면 외치고 종료
         if (cnt >= 3) {
             System.out.println(idx + 1);
             System.exit(0); // 프로그램 종료
         }
     }
-
+    
     public static void main(String[] args) throws Exception {
-        // 1. 입력
+        //1. 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         for (int i = 0; i < 5; i++) {
@@ -78,12 +122,12 @@ public class Main {
         }
         cnt = 0;
 
-        // 2. 처리
+        //2. 처리
         for (int s = 0; s < say.length; s++) {
-            bingo(s, say[s]); // idx,val
+            bingo(s, say[s]); //idx,val
         }
-
-        // 3. 출력
+        
+        //3. 출력
         br.close();
     }
 }
